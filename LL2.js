2f1fe3186b1839313a835a8baa65cde53d22d16e
Iqbal Erani <iqbalerani@gmail.com>
2023-05-15 17:27:58 +0500

    2 commit

--- LL2.js
+++ LL2.js
@@ -1,3 +1,59 @@
+const LinkedList = require('./LinkedList');
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    add(value) {
+      const newNode = new Node(value);
+  
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        let currentNode = this.head;
+        while (currentNode.next !== null) {
+          currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+      }
+    }
+  
+    remove(value) {
+      if (this.head === null) {
+        return;
+      }
+  
+      if (this.head.value === value) {
+        this.head = this.head.next;
+        return;
+      }
+  
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        if (currentNode.next.value === value) {
+          currentNode.next = currentNode.next.next;
+          return;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+  
+    forEach(callback) {
+      let currentNode = this.head;
+      while (currentNode !== null) {
+        callback(currentNode);
+        currentNode = currentNode.next;
+      }
+    }
+  }   
+// Create a new linked list
 const myList = new LinkedList();
 
 // Add some nodes to the list
@@ -17,3 +73,4 @@ myList.remove(1);
 myList.forEach(node => {
   console.log(node.value); // 2, 3
 });
+
---
+++ Searching.js
@@ -0,0 +1,3 @@
+const arr = [2, 4, 6, 8, 10];
+console.log(arr.indexOf(6)); // 2
+console.log(arr.indexOf(5)); // -1

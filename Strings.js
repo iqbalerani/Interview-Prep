40339282ec50ef6bf7bfd5701172afaf295dbdf6
Iqbal Erani <iqbalerani@gmail.com>
2023-05-15 15:29:20 +0500

    2 commit

---
+++ Strings.js
@@ -0,0 +1,41 @@
+// Double quotes
+let message1 = "Hello, world!";
+
+// Single quotes
+let message2 = 'Welcome to JavaScript!';
+
+// Backticks (template literals)
+let name = "Alice";
+let greeting = `Hello, ${name}!`;
+
+// console.log(greeting);
+
+// console.log(name.length);
+
+// console.log(name[0]);
+// console.log(name.charAt(4));
+
+// for (i = 0; i < name.length; i++) {
+//     console.log(name[i]);
+// }
+
+let firstName = "John";
+let lastName = "Doe";
+
+// let fullname = firstName + " " + lastName
+// console.log(fullname);
+
+// console.log(fullname.toUpperCase());
+// console.log(fullname.toLowerCase());
+// console.log(fullname.indexOf("oe"));
+// console.log(fullname.includes("oe"));
+// console.log(fullname.replace("Doe", "Smith"));
+
+let str = "JavaScript is awesome!";
+
+console.log(str.length);
+let sliceStr = str.slice(14,22);
+console.log(sliceStr);
+
+let splitStr = str.split("i");
+console.log(splitStr);

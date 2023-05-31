caeff37694581e182c1d09f522be2e3f1ff9a53d
Iqbal Erani <iqbalerani@gmail.com>
2023-05-31 14:56:10 +0500

    310523

--- Concatenation of Array.js
+++ Concatenation of Array.js
@@ -1,15 +1,26 @@
 const nums = [1,3,2,1]
 // Output: [1,2,1,1,2,1]
 
-const getConcatenation = function(nums) {
-    let con = []
-    for(let i = 0; i < nums.length; i++) {
-        con.push(nums[i])
-    }
-    for(let i = 0; i < nums.length; i++) {
-        con.push(nums[i])
-    }
-    return con
+// const getConcatenation = function(nums) {
+//     let con = []
+//     for(let i = 0; i < nums.length; i++) {
+//         con.push(nums[i])
+//     }
+//     for(let i = 0; i < nums.length; i++) {
+//         con.push(nums[i])
+//     }
+//     return con
+// };
+
+// var getConcatenation = function(nums) {
+//     nums.push(...nums);
+//     return nums;
+// };
+
+function getConcatenation(nums) {
+    let ans = [];
+        ans = ans.concat(nums, nums);
+    return ans;
 };
 
 console.log(getConcatenation(nums));

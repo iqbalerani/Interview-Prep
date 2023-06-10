0fd6dafa22a60e3628682ca0039cca612ddb1ade
Iqbal Erani <iqbalerani@gmail.com>
2023-06-10 15:16:58 +0500

    10 Jun 23

---
+++ sortColors.js
@@ -0,0 +1,11 @@
+// let nums = [2,0,2,1,1,0]
+let nums = [2,0,1]
+// Output: [0,0,1,1,2,2]
+
+
+let sortColors = function(nums) {
+    let sortC = nums.sort()
+    return sortC
+};
+
+console.log(sortColors(nums));

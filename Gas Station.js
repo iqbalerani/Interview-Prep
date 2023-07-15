f4c1b977647463a30e8bd9891a2bdfad45521fa7
Iqbal Erani <iqbalerani@gmail.com>
2023-07-15 15:02:57 +0500

    15 JUL 23

--- Gas Station.js
+++ Gas Station.js
@@ -3,7 +3,13 @@ let cost = [3,4,5,1,2];
 // Output: 3
 
 const canCompleteCircuit = function(gas, cost) {
-    
+    let totalGas = 0;
+    let currectGas = 0;
+
+    for(let i; i < gas.length; i++){
+        totalGas = cost[i] - gas[i];
+        if()
+    }
 };
 
 console.log(canCompleteCircuit(gas, cost));
